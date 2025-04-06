443b4eba2345c3c3dcbfe8f371474e623c1fa3ee
Jedidta Adoni Saputra <adoni.091203@gmail.com>
2025-04-06 17:51:46 +0700

    Revert "feat: update .gitignore"
    This reverts commit b87b87e4b1b775aa1cccfc500f67a108c1fccfae.

--- Assets/Scripts/StateMachine/States/PlayerStateMachine.cs
+++ Assets/Scripts/StateMachine/States/PlayerStateMachine.cs
@@ -3,107 +3,55 @@ using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
 [RequireComponent(typeof(InputHandler))]
-[RequireComponent(typeof(Animator))]
+
 public class PlayerStateMachine : StateMachine
 {
     [Header("Movement Settings")]
-    [SerializeField] private float _walkSpeed = 2f;
-    [SerializeField] private float _slowRunSpeed = 4f;
-    [SerializeField] private float _runSpeed = 8f;
-    [SerializeField] private float _acceleration = 5f;
-    [SerializeField] private float _jumpForce = 8f;
-    [SerializeField] private float _gravity = -25f;
-    [SerializeField] private float _airControl = 3f;
-    [SerializeField] private float _maxSlopeAngle = 45f;
-    
-    [Header("Scene Settings")]
-    [SerializeField] private bool _walkScene = false;
-    [SerializeField] private bool _runScene = true;
-    
-    [Header("Ground Detection")]
-    [SerializeField] private LayerMask _groundLayer;
+    public float currentSpeed;
+    public float acceleration = 5f;
+    public float walkSpeed;
+    public float slowRunSpeed;
+    public float runSpeed;
+    public float jumpForce = 8f;
+    public float gravity = -25f;
+    public float airControl = 3f;
+    public bool walkScene = false;
+    public bool runScene = true;
+    public bool isRunning;
 
     [Header("References")]
     public CharacterController controller;
     public InputHandler inputHandler;
     public Camera playerCamera;
-    public Animator animator;
 
-    // State
-    public PlayerStateFactory StateFactory { get; private set; }
-    public float currentSpeed;
     public Vector3 movementInput;
     public bool jumpTriggered;
     public float verticalVelocity;
-    public bool isRunning;
+
+    public Animator Animator;
+
     
-    // Properties
-    public float WalkSpeed => _walkSpeed;
-    public float SlowRunSpeed => _slowRunSpeed;
-    public float RunSpeed => _runSpeed;
-    public float Acceleration => _acceleration;
-    public float JumpForce => _jumpForce;
-    public float Gravity => _gravity;
-    public float AirControl => _airControl;
-    public bool WalkScene => _walkScene;
-    public bool RunScene => _runScene;
-    public float MaxSlopeAngle => _maxSlopeAngle;
-    public LayerMask groundLayer => _groundLayer;
-    
-    private void Awake()
+    private void Start()
     {
         controller = GetComponent<CharacterController>();
         inputHandler = GetComponent<InputHandler>();
-        animator = GetComponent<Animator>();
+        Animator = GetComponent<Animator>();
         
         if (playerCamera == null)
         {
             playerCamera = Camera.main;
         }
         
-        StateFactory = new PlayerStateFactory(this);
-    }
-    
-    private void Start()
-    {
-        ChangeState(StateFactory.Idle());
-        
-        // Subscribe to events from InputHandler
-        if (inputHandler != null)
-        {
-            inputHandler.OnJumpPressed += HandleJumpInput;
-            inputHandler.OnRunPressed += HandleRunInput;
-        }
+        ChangeState(new IdleState(this));
     }
 
     private void OnDestroy()
     {
-        // Unsubscribe from events
-        if (inputHandler != null)
-        {
-            inputHandler.OnJumpPressed -= HandleJumpInput;
-            inputHandler.OnRunPressed -= HandleRunInput;
-        }
-    }
-    
-    protected override void Update()
-    {
-        // Process input
-        if (inputHandler != null)
-        {
-            movementInput = inputHandler.MovementValue;
-        }
         
-        base.Update();
     }
-    
-    private void HandleJumpInput()
-    {
-        jumpTriggered = true;
-    }
-    
-    private void HandleRunInput(bool isPressed)
-    {
-        isRunning = isPressed && !_walkScene;
-    }
+
+    // private void Update()
+    // {
+    //     jumpTriggered = false;
+    // }
 }
